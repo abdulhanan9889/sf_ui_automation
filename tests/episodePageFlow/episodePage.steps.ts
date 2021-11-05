27540df7f68da901c7fa381dac9ef768becd1e55
mah-noor9 <92865937+mah-noor9@users.noreply.github.com>
2021-11-05 19:00:17 +0500

    Update episodePage.steps.ts

--- tests/episodePageFlow/episodePage.steps.ts
+++ tests/episodePageFlow/episodePage.steps.ts
@@ -1,19 +1,19 @@
 import { Given, When, After, Then, AfterAll, AfterStep } from '@cucumber/cucumber'
 import { loadBrowser } from '../utilities/loadBrowser'
-import { fillSignUpForms, loginThroughSignedUpUser, loginThroughTrailblazerId, logoutFromSFPlatform, maximizeVideoPlayer, minimizeVideoPlayer, openAuthorizedEpisode } from './episodePage.tasks'
-import { verifyEpisodeNumber, verifySeriesTitle, verifyEpisodeTitle, verifySpeakerOneDetails, verifySpeakerTwoDetails, verifySpeakerThreeDetails, verifyForwardedVideo, verifyReversedVideo, verifyMutedVideo, verifyUnmutedVideo, verifyMaximizedPlayer, verifyMinimizedPlayer } from './episodePage.assertions'
-import { acceptCookies } from '../unAuthenticatedFlow/unAuthFlow.actions'
-import { verifyProgressBarValues } from '../unAuthenticatedFlow/unAuthFlow.assertions'
+import { fillSignUpForms, loginThroughSignedUpUser, loginThroughTrailblazerId, logoutFromSFPlatform, maximizeVideoPlayer, minimizeVideoPlayer, openAuthorizedEpisode } from '../tasks/episodePage.tasks'
+import { verifyEpisodeNumber, verifySeriesTitle, verifyEpisodeTitle, verifySpeakerOneDetails, verifySpeakerTwoDetails, verifySpeakerThreeDetails, verifyForwardedVideo, verifyReversedVideo, verifyMutedVideo, verifyUnmutedVideo, verifyMaximizedPlayer, verifyMinimizedPlayer } from '../assertions/episodePage.assertions'
+import { acceptCookies, clickSecondEpisodeButton } from '../actions/unAuthFlow.actions'
+import { verifyProgressBarValues } from '../assertions/unAuthFlow.assertions'
 import { waitTillHTMLRendered } from '../utilities/waitTillHTMLRendered'
-import { isUserLoggedOut } from '../broadcastPageFlow/broadcastPage.assertions'
+import { isUserLoggedOut } from '../assertions/broadcastPage.assertions'
 import SFDataInsertion from '../testDataGeneration/testDataLogic/SFDataInsertion'
 import BaseObject from '../testDataGeneration/entities/BaseObject'
 import SFDataLogic from '../testDataGeneration/testDataLogic/testDataLogic'
-import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
 
-import { openEpisode, playEpisode } from '../unAuthenticatedFlow/unAuthFlow.tasks'
-import { muteVideoButton, unmuteVideoButton } from '../broadcastPageFlow/broadcastPage.actions'
-import { clickSecondAuthorizedEpisodeButton } from './episodePage.actions'
+
+import {  testData,testDataSet,destroy,openEpisode, playEpisode } from '../tasks/unAuthFlow.tasks'
+import { muteVideoButton, unmuteVideoButton } from '../actions/broadcastPage.actions'
+import { clickSecondAuthorizedEpisodeButton } from '../actions/episodePage.actions'
 
 var { setDefaultTimeout } = require('@cucumber/cucumber');
 setDefaultTimeout(80000)
@@ -23,13 +23,14 @@ let recorder
 
 Given('user generates data for authenticated epsiode flows', async function (datatable) {
     const testDataParameters = await datatable.hashes()[0]
-    // await SFDataInsertion.createOriginalSeriesWithEpisodes(testDataParameters.numberOfSeries, testDataParameters.numberOfEpisodesPerSeries, testDataParameters.seriesStartDayFromToday, testDataParameters.seriesEndDayFromToday)
+    await testData(testDataParameters.numberOfEpisodesPerSeries, 
+        testDataParameters.seriesStartDayFromToday, testDataParameters.seriesEndDayFromToday, testDataParameters.numberOfSpeakers)
+      
 })
 
-Given('user generates data for unauthenticated epsiode flows', async function (datatable) {
-    const testDataParameters = await datatable.hashes()[0]
-    // await SFDataInsertion.createEventFlowHavingSeriesWithEpisodes(testDataParameters.numberOfSeries, testDataParameters.numberOfEpisodesPerSeries, testDataParameters.eventStartDayFromToday, testDataParameters.eventStartHour, testDataParameters.eventEndDayFromToday, testDataParameters.eventEndHour)
-})
+// Given('user generates data for unauthenticated epsiode flows', async function (datatable) {
+//     const testDataParameters = await datatable.hashes()[0]
+// })
 
 Given('a guest user loads salesforce plus platform', async function () {
     page = await loadBrowser()
@@ -97,46 +98,46 @@ Then('user can mute and unmute the video', async function () {
     // await recorder.stop()
 })
 
-When('a guest user access authorized content and logs in through trailblazer id', async function () {
-    await openAuthorizedEpisode(page)
-    await loginThroughTrailblazerId(page)
-})
+// When('a guest user access authorized content and logs in through trailblazer id', async function () {
+//     await openAuthorizedEpisode(page)
+//     await loginThroughTrailblazerId(page)
+// })
 
-When('a guest user fills out the sign up forms', async function (datatable) {
-    const dataFields = await datatable.hashes()[0];
-    await fillSignUpForms(page, dataFields)
-})
+// When('a guest user fills out the sign up forms', async function (datatable) {
+//     const dataFields = await datatable.hashes()[0];
+//     await fillSignUpForms(page, dataFields)
+// })
 
-Then('an authenticated user can play the authorized episode', async function () {
-    await playEpisode(page)
-    await verifyProgressBarValues(page)
-    // recorder.stop()
-})
+// Then('an authenticated user can play the authorized episode', async function () {
+//     await playEpisode(page)
+//     await verifyProgressBarValues(page)
+//     // recorder.stop()
+// })
 
-Then('authenticated user can play the first authorized episode', async function () {
-    await playEpisode(page)
-    await verifyProgressBarValues(page)
-})
+// Then('authenticated user can play the first authorized episode', async function () {
+//     await playEpisode(page)
+//     await verifyProgressBarValues(page)
+// })
 
-Then('authenticated user clicks on second episode and can play the authorized episode', async function () {
-    await clickSecondAuthorizedEpisodeButton(page)
-    await playEpisode(page)
-    await verifyProgressBarValues(page)
-    // await recorder.stop()
-})
+// Then('authenticated user clicks on second episode and can play the authorized episode', async function () {
+//     await clickSecondAuthorizedEpisodeButton(page)
+//     await playEpisode(page)
+//     await verifyProgressBarValues(page)
+//     // await recorder.stop()
+// })
 
-When('a guest user access authorized content and logs in through trailblazer id: {word}', async function (emailId) {
-    await loginThroughSignedUpUser(page, emailId)
-})
+// When('a guest user access authorized content and logs in through trailblazer id: {word}', async function (emailId) {
+//     await loginThroughSignedUpUser(page, emailId)
+// })
 
-When('a guest user fills out the sign up forms and clicks cancel and logout button', async function (datatable) {
-    const dataFields = await datatable.hashes()[0];
-    await logoutFromSFPlatform(page, dataFields)
-})
+// When('a guest user fills out the sign up forms and clicks cancel and logout button', async function (datatable) {
+//     const dataFields = await datatable.hashes()[0];
+//     await logoutFromSFPlatform(page, dataFields)
+// })
 
-Then('the user is logged out', async function () {
-    await isUserLoggedOut(page);
-})
+// Then('the user is logged out', async function () {
+//     await isUserLoggedOut(page);
+// })
 AfterStep("@episodePage", async function () {
     await waitTillHTMLRendered(page);
     ss = await page.screenshot({ fullPage: true })
@@ -148,6 +149,5 @@ After("@episodePage", async function () {
 })
 
 AfterAll(async function () {
-    let baseobject = new BaseObject()
-    // SFDataLogic.deleteRecord(baseobject.getObjectId(), baseobject.getObjectName())
+    await destroy()
 })
