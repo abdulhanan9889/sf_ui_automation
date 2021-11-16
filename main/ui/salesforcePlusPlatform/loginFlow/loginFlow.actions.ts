35432f7b686ebe73cbd27484b3d917a4d0907e58
mbinjaved <mbinjaved@salesforce.com>
2021-11-16 10:31:45 +0500

    Deleted deprecated files

--- main/ui/salesforcePlusPlatform/loginFlow/loginFlow.actions.ts
+++
@@ -1,7 +0,0 @@
-import { getTrailblazzerMeButton } from "./user_interface/trailBlazzerModalSelectors";
-
-export async function clickTrailblazzerButton(page) {
-    await page.waitForSelector(getTrailblazzerMeButton)
-    let trailblazzerMeButton = await page.$(getTrailblazzerMeButton);
-    await trailblazzerMeButton.click();
-}
